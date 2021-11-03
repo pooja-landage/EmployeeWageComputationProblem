7a04eb1095edbd9bcaaf5cf0d8d762fe977e553d
Pooja <poojalandage22@gmail.com>
2021-11-03 15:45:21 +0530

    Added : Part time wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-isPresent=1;
-randomCheck=$((RANDOM%2))
-if [ $isPresent -eq $randomCheck ];
-then
-   empRatePerHrs=20
-   empHrs=8
-   salary=$(($empHrs*$empRatePerHrs))
-else
-   salary=0
-fi
+isFullTime=1;
+isPartTime=2;
+employeeRate=20;
+randomCheck=$((RANDOM%3))
+case $randomCheck in
+   $isFullTime)
+      echo Fulltime
+      employeeHrs=8;;
+   $isPartTime)
+      echo Parttime
+      employeeHrs=4;;
+   *)
+      echo absent
+      employeeHrs=0;;
+esac
+
+salary=$(( $employeeHrs*$employeeRate ))
+echo "Salary= $salary"
